56cacea9d9db04238d8e13a88c79644a5339ef6e
Stefan Lang <Stefan.Lang@med.lu.se>
2023-04-26 10:11:08 +0200

    Hope this gets updated now.

--- workshop/target/release/deps/workshop-45f405a6624ef1cf.d
+++
@@ -1,5 +0,0 @@
-/home/med-sal/git_Projects/Rust_Programming_1/workshop/target/release/deps/workshop-45f405a6624ef1cf: src/main.rs
-
-/home/med-sal/git_Projects/Rust_Programming_1/workshop/target/release/deps/workshop-45f405a6624ef1cf.d: src/main.rs
-
-src/main.rs:
--- workshop/target/release/workshop.d
+++
@@ -1,1 +0,0 @@
-/home/med-sal/git_Projects/Rust_Programming_1/workshop/target/release/workshop: /home/med-sal/git_Projects/Rust_Programming_1/workshop/src/main.rs
